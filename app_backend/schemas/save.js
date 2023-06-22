8909bb1d8c11119fc2c4a074a9bfa3fdacd3c1a3
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-06-23 00:25:20 +0530

    save.js done

---
+++ app_backend/schemas/save.js
@@ -0,0 +1,18 @@
+export default
+{
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedby',
+            title: 'PostedBy',
+            type: 'postedby'
+        },
+        {
+            name: 'userid',
+            title: 'UserID',
+            type: 'string'
+        }
+    ]
+}
