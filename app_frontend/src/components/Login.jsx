d66b85a68a8308c8571229d33b6acd22e0d0ddab
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-06-27 06:59:14 +0530

    Imported assets

---
+++ app_frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FcGoogle } from 'react-icons'
+import { useNavigate } from 'react-router-dom'
+import share
+
+const Login = () => {
+  return (
+    <div>
+        Login
+    </div>
+  )
+}
+
+export default Login
