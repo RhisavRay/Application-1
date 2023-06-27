cced2430b807c4f5741f372d31d9f491b83bbe40
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-06-27 07:14:49 +0530

    Login page being set up

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
+import Login from './components/Login'
+
+import Home from './container/Home'
 
 const App = () => {
+/*
+  const handleCallbackResponse = (response) =>
+  {
+
+  }
+
+  useEffect(() => {
+    /* global google
+    google.accounts.id.initialize({
+      client_id: "555889186138-mjdk21rvv719jlsajdumqhns886e20m3.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    )
+  }, [])
+*/
   return (
-    <div className='text-3xl font-bold underline'>
-        Hello World
-    </div>
+    <Routes>
+      <Route path='login' element={<Login/>}/>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
   )
 }
 
--- app_frontend/src/components/Login.jsx
+++ app_frontend/src/components/Login.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons'
 import { useNavigate } from 'react-router-dom'
-import share
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logowhite.png'
 
 const Login = () => {
   return (
-    <div>
-        Login
+    <div className = "flex justify-start items-center flex-col h-screen">
+
+        <div className='relative w-full h-full'>
+
+          <video src={shareVideo} type="video/mp4" loop controls={false} muted autoPlay className='w-full h-full object-cover'/>
+          <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+
+            <div className='b-5'>
+
+              <img src={logo} width='130px' alt='App Logo'/>
+
+            </div>
+            <div className='shadow-2xl'>
+
+              .
+
+            </div>
+
+          </div>
+
+        </div>
+
     </div>
   )
 }
