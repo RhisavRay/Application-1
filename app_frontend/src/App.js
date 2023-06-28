d39bd992cc319a19929b59ed240750881988ced8
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-06-28 13:56:58 +0530

    .

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,31 +1,16 @@
-import React, { useEffect } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './container/Home'
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
-import jwt_decode from 'jwt-decode'
 
 const App = () => {
 
-  return(
-    <GoogleOAuthProvider clientId='555889186138-mjdk21rvv719jlsajdumqhns886e20m3.apps.googleusercontent.com'>
-      <GoogleLogin onSuccess={(credentialResponse) => {
-        var decoded = jwt_decode(credentialResponse.credential)
-        console.log(decoded)
-      }} onError={() => {
-        console.log("Login Failed") 
-      }}/>
-    </GoogleOAuthProvider>
-  )
-
-  /*
   return (
     <Routes>
       <Route path='login' element={<Login/>}/>
       <Route path='/*' element={<Home/>}/>
     </Routes>
   )
-  */
 }
 
 export default App
--- app_frontend/src/components/Login.jsx
+++ app_frontend/src/components/Login.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { FcGoogle } from 'react-icons'
 import { useNavigate } from 'react-router-dom'
 import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import jwt_decode from 'jwt-decode'
+import { client } from '../client'
 
 const Login = () => {
+
+  const navigate = useNavigate()
+  const GoogleResponse = (credentialResponse) =>
+  {
+    var decoded = jwt_decode(credentialResponse.credential)
+
+    const { name, sub, picture } = decoded
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture
+    }
+
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true })
+    })
+  }
+
   return (
     <div className = "flex justify-start items-center flex-col h-screen">
 
@@ -18,9 +40,13 @@ const Login = () => {
               <img src={logo} width='130px' alt='App Logo'/>
 
             </div>
-            <div className='shadow-2xl'>
+            <div className='shadow-2xl p-2'>
 
-              .
+              <GoogleOAuthProvider clientId='555889186138-mjdk21rvv719jlsajdumqhns886e20m3.apps.googleusercontent.com'>
+                <GoogleLogin onSuccess={GoogleResponse} onError={() => {
+                  console.log("Login Failed") 
+                }}/>
+              </GoogleOAuthProvider>
 
             </div>
 
